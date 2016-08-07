acace62aae29dcedd5ad2d7623781795048edba3
Nitesh Patel <nitesh.patel@hotmail.com>
2016-08-07 10:50:24 +0100

    Run code coverage with karma (uses Istanbul under the hood)

--- karma.conf.js
+++ karma.conf.js
@@ -35,8 +35,12 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
+        coverageReporter: {
+            type: 'lcovonly',
+            dir: 'coverage/'
+        },
 
         // web server port
         port: 9876,

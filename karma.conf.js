91f8b8da6e86da7c0027ea247dadb95614ba417f
Nitesh Patel <nitesh.patel@hotmail.com>
2016-08-07 12:14:11 +0100

    Move jquery to lib folder, to exclude from our own code; fix issue in preprocessor paths

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'js/src/jquery-3.1.0.min.js',
+            'js/lib/*.js',
             'js/src/*.js',
             'js/test/*.js'
         ],
@@ -43,7 +43,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            '**/js/*.js': ['coverage']
+            'js/src/*.js': ['coverage']
         },
 
 

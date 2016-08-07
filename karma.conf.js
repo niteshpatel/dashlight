96cd8f3f7767e791184544205866f8aeeb72b3c3
Nitesh Patel <nitesh.patel@hotmail.com>
2016-08-07 12:00:03 +0100

    Add missing preprocessors sectino

--- karma.conf.js
+++ karma.conf.js
@@ -27,24 +27,28 @@ module.exports = function (config) {
         exclude: [],
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
-
-
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage'],
 
+
+        // coverage reporters
         coverageReporter: {
             type: 'lcovonly',
             dir: 'coverage/'
         },
 
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            '**/js/*.js': ['coverage']
+        },
+
+
         // web server port
         port: 9876,
-
 
         // enable / disable colors in the output (reporters and logs)
         colors: true,

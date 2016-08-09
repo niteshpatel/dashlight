863dd19d7549bae7c4d52d69e83b2584085b740f
Nitesh Patel <nitesh.patel@hotmail.com>
2016-08-09 18:00:00 +0100

    Url must be https to work in Edge (for some reason the internal redirect doesn't kick in the same way it does on Chrome)

--- source/samples/providers.js
+++ source/samples/providers.js
@@ -11,7 +11,7 @@ callback(
             name: "travis_dashlight_provider",
             widget: "build_status",
             plugin: "travis_build_status",
-            url: "http://api.travis-ci.org/repos/niteshpatel/dashlight/builds",
+            url: "https://api.travis-ci.org/repos/niteshpatel/dashlight/builds",
             headers: {
                 "Accept": "application/vnd.travis-ci.2+json"
             },
